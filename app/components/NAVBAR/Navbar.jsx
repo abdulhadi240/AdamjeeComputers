a250ee47d2069eaf046d12ea8d77ef3d4cf54eea
abdulhadi240 <ah912425@gmail.com>
2023-07-27 15:55:33 +0500

    about linked

--- app/components/NAVBAR/Navbar.jsx
+++ app/components/NAVBAR/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div className=" lg:gap-4 text-black hidden lg:flex">
             <Name text={'Home'}  link={'/'}/>
             <Name text={'Contracts'} link={'/LABS'}/> 
-            <Name text={'About us'} link={'/'}/> 
+            <Name text={'About us'} link={'/ABOUT'}/> 
             <Name text={'Contact us'} link={'/CONTACT'}/>  
         </div>
         <div><button className="w-auto h-auto p-3 text-sm bg-gray-400 text-white  rounded-lg hover:bg-gray-700 hover:text-white transition-all">SHOP NOW</button></div>
