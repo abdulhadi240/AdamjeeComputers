9c738cd4990bbc61693be724f3f1bf4bf084c797
abdulhadi240 <ah912425@gmail.com>
2023-07-30 19:54:56 +0500

    contact

--- app/components/ContactCard1.jsx
+++ app/components/ContactCard1.jsx
@@ -8,17 +8,17 @@ const ContactCard1 = ({ Ricons, Heading, text, link, label }) => {
             <div className='w-9 h-auto p-1 rounded-lg border-[1px] transition-all ml-4'>
                 <Ricons size={24} color='black' />
             </div>
-            <div className='flex flex-col gap-1 mt-20 group-hover:text-white transition-all ml-4'>
+            <div className='flex flex-col gap-1 mt-16 group-hover:text-white transition-all ml-4'>
                 <div className='text-lg font-bold '>{Heading}</div>
                 <div className='text-xs mt-1'>{text}</div>
                 <div className='flex flex-col'>
                     <div className='underline mt-6 text-sm font-semibold'>
                         Moiz : 03085066630
                     </div>
-                    <div className='underline mt-6 text-sm font-semibold'>
+                    <div className='underline mt-1 text-sm font-semibold'>
                         Moiz : 03085066630
                     </div>
-                    <div className='underline mt-6 text-sm font-semibold'>
+                    <div className='underline mt-1 text-sm font-semibold'>
                         Moiz : 03085066630
                     </div>
 
