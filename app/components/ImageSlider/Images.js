8ebd5d415fb9e4935bc2825a441821a9768c2508
abdulhadi240 <ah912425@gmail.com>
2023-08-02 01:27:43 +0500

    image slider finished

--- app/components/ImageSlider/Images.js
+++ app/components/ImageSlider/Images.js
@@ -1,10 +1,11 @@
 import image1 from './assests/1.jpg'
 import image2 from './assests/2.jpg'
 import image3 from './assests/3.jpg'
-import image4 from './assests/4.jpg'
-import image5 from './assests/5.jpg'
-import image6 from './assests/6.jpg'
+import image4 from './assests/41.jpg'
+import image5 from './assests/42.jpg'
+import image6 from './assests/43.jpg'
+import image7 from './assests/44.jpg'
 
 
 
-export default [image1 , image2 , image3 ,image4,image5 , image6]
+export default [image1 , image2 , image3 ,image4,image5 , image6,image7]
